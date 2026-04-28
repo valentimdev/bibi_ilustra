841f628904a5715523e5f29efc77db4e0495ab3d
valentimdev <valentimdesenvolvedor@gmail.com>
2026-04-28 16:23:09 -0300

    feat: changes slug style

--- src/app/work/[slug]/page.tsx
+++ src/app/work/[slug]/page.tsx
@@ -38,8 +38,8 @@ export default async function ProjectPage({ params }: ProjectPageProps) {
   return (
     <section className="w-full pt-10 pb-10 ">
 
-      <header className="text-center  py-4 mx-auto">
-        <div className=" flex items-center   justify-center relative  ">
+      <header className="text-center  py-4 mx-auto border border-purple-700">
+        <div className=" flex items-center  justify-center relative  border border-red-600">
           <div className="absolute left-0">
             <Link href="/">
               <button className="cursor-pointer">
@@ -55,26 +55,29 @@ export default async function ProjectPage({ params }: ProjectPageProps) {
           </div>
           
       
-          <div className="flex justify-center ">
+          <div className="flex flex-col justify-center border border-amber-400">
             <h1 className="text-4xl sm:text-5xl font-bold tracking-tight text-gray-900">
               {project.title}
             </h1>
+            <p className="mt-2 text-lg text-gray-500  ">{project.date}</p>
           </div>
-          
         </div>
+          
       
-        <p className="mt-2 text-lg text-gray-500  ">{project.date}</p>
-        <p className="mt-2 text-xl text-center text-gray-800 mx-auto whitespace-pre-wrap">
+        
+        <div className='border-yellow-500 flex px-20 '>
+        <p className="mt-2 text-xl text-justify   text-gray-800 mx-auto ">
           {project.description}
         </p>
+        </div>
       </header>
 
       <div className="space-y-1 md:space-y-1 mb-10 ">
         {project.muralSections.map((section, index) => (
           <div key={index}>
             {section.type === 'full' && (
-              <div className="w-full flex justify-center ">
-                <div className="w-full ">
+              <div className="w-full flex justify-center border items-center border-amber-950">
+                <div className="w-full flex justify-center border items-center px-20">
                   {isVideo(section.imageUrl) ? (
                     <video
                       src={section.imageUrl}
